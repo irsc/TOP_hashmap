a81782862ffea6b90ddc354b626153273c985def
irsc <rivero.scastilla@gmail.com>
2024-09-02 11:59:07 +0200

    Methods for clear, keys and values added

--- src/hashmap.js
+++ src/hashmap.js
@@ -40,33 +40,84 @@ export class HashMap {
     checkBucketGrowth(){
         let numberOfEntries = 0;
         if(this.capacity*this.loadFactor < numberOfEntries){
-          let extraBuckets = Array.apply(null, Array(this.capacity)).map(function () { });
-          this.buckets =  this.buckets.concat(extraBuckets);
+            let extraBuckets = Array.apply(null, Array(this.capacity)).map(function () { });
+            this.buckets = this.buckets.concat(extraBuckets);
+            this.capacity = this.buckets.length;
       }
     }
 
     // Takes one argument as a key and returns the value that is assigned to this key. 
     //If a key is not found, return null.
     get(key){
-      let index = this.hash(key);
-      if(index >= 0){
-        let existingList = this.buckets[index];
-        let indexKey = existingList.findKey(key);
-        return existingList.at(indexKey).value[key]
-        
-      }else{
-          return null;
-      }
+        let index = this.hash(key);
+        if(index >= 0){
+            let existingList = this.buckets[index];
+            let indexKey = existingList.findKey(key);
+            return existingList.at(indexKey).value[key];
+          
+        }else{
+            return null;
+        }
     }
 
     //Takes a key as an argument and returns true or false based on whether or not the key is in the hash map.
     has(key){
-      let bucket = this.buckets[this.hash(key)];
-      if(bucket != undefined){
-        return bucket.containsKey(key);
-      }
-      return false;
+        let bucket = this.buckets[this.hash(key)];
+        if(bucket != undefined){
+          return bucket.containsKey(key);
+        }
+        return false;
     }
+    // Takes a key as an argument. If the given key is in the hash map, it should remove the entry with that key and return true. 
+    // If the key isn’t in the hash map, it should return false.
+    remove(key){
+      if(this.has(key)){
+          let existingList = this.buckets[this.hash(key)];
+          let indexKey = existingList.findKey(key);
+          existingList.removeAt(indexKey);
+          return true;
+      }else{
+        return false;
+      }
+    }
+    // Returns the number of stored keys in the hash map.
+    length(){
+        let numberKeys = 0;
+        this.buckets.forEach(bucket => {
+            if(bucket != undefined){
+                numberKeys += bucket.getSize();
+            }
+        });
+        return numberKeys;
+    }
+    // Removes all entries in the hash map.
+    clear(){
+        this.buckets = Array.apply(null, Array(this.capacity)).map(function () { });
+    }
+    // Returns an array containing all the keys inside the hash map.
+    keys(){
+        let arrayKeys = [];
+        this.buckets.forEach(bucket => {
+            if(bucket != undefined){
+                arrayKeys.concat(bucket.getKeys());
+            }
+        });
+        return arrayKeys;
+    }
+    // Returns an array containing all the values.
+    values(){
+      let arrayValues = [];
+      this.buckets.forEach(bucket => {
+          if(bucket != undefined){
+              arrayValues.concat(bucket.getValues());
+          }
+      });
+      return arrayValues;
+    }
+    // Returns an array that contains each key, value pair. 
+    //Example: [[firstKey, firstValue], [secondKey, secondValue]]
+    entries(){
 
+    }
 
 }
